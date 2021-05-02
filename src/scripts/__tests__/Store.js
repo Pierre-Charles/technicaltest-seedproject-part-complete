ffa97def1c00fdef1aba860d18980741d35dad5e
Pierre Charles <p.charles0501@gmail.com>
2021-05-02 22:10:44 +0100

    Setting up tests

--- src/scripts/__tests__/Store.js
+++ src/scripts/__tests__/Store.js
@@ -2,6 +2,14 @@ import Store from "../Store";
 import mockData from "../../../public/db.json";
 
 describe("filter", () => {
+  let store;
+  beforeEach(() => {
+    store = new Store();
+    store.setDeals(mockData);
+  });
+
+  // Tests that meet filter criteria
+
   it("should return all deals when no filters applied", () => {
     // Arrange
     const sut = new Store();
@@ -13,4 +21,29 @@ describe("filter", () => {
     // Assert
     expect(result).toEqual(mockData.deals);
   });
+
+  it("should return the 4 deals for broadband only when filtering by broadband", () => {});
+
+  it("should return the 4 deals for broadband and TV only when filtering by broadband and TV", () => {});
+
+  it("should return the 1 deal for broadband and mobile when filtering by broadband and movile", () => {});
+
+  it("should return the 1 deal for Sky only when filtering by Sky", () => {});
+
+  it("should return the 2 deals for BT with broadband and TV only when filtering by BT, broadband and TV", () => {});
+
+  // Extra tests
+  it("should return nothing when filtering by mobile only", () => {});
+
+  it("should return nothing when filtering by TV only", () => {});
+
+  it("should return nothing when filtering by mobile and TV only", () => {});
+
+  it("should return only 3 deals for Plusnet when filtering by Plusnet", () => {});
+
+  it("should return the 1 deal for EE only when filtering by EE", () => {});
+
+  it("should return 5 deals for BT only when filtering by BT", () => {});
+
+  it("should return the 1 deal for Origin Broadband only when filtering by Origin Broadband", () => {});
 });
