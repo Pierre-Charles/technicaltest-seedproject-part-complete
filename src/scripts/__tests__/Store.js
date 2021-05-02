c7e0b6b214806de8f495d752999cae0823e638dd
Pierre Charles <p.charles0501@gmail.com>
2021-05-02 23:19:06 +0100

    Adding tests for provider filtering

--- src/scripts/__tests__/Store.js
+++ src/scripts/__tests__/Store.js
@@ -1,15 +1,17 @@
 import Store from "../Store";
 import mockData from "../../../public/db.json";
+import getProviderId from "../helpers/getProviderId";
 
 describe("filter", () => {
   let store;
+
   beforeEach(() => {
+    // Arrange
     store = new Store();
     store.setDeals(mockData.deals);
   });
 
   // Tests that meet filter criteria
-
   it("should return all deals when no filters applied", () => {
     // Act
     const { deals } = store;
@@ -48,9 +50,25 @@ describe("filter", () => {
     expect(deals).toHaveLength(1);
   });
 
-  it("should return the 1 deal for Sky only when filtering by Sky", () => {});
+  it("should return the 1 deal for Sky only when filtering by Sky", () => {
+    // Arrange
+    store.setProviderFilter(getProviderId("sky"));
+    const { deals } = store;
 
-  it("should return the 2 deals for BT with broadband and TV only when filtering by BT, broadband and TV", () => {});
+    // Assert
+    expect(deals).toHaveLength(1);
+  });
+
+  it("should return the 2 deals for BT with broadband and TV only when filtering by BT, broadband and TV", () => {
+    // Act
+    store.setProviderFilter(getProviderId("bt"));
+    store.setProductFilter("Broadband");
+    store.setProductFilter("Tv");
+    const { deals } = store;
+
+    // Assert
+    expect(deals).toHaveLength(2);
+  });
 
   // Extra tests
   it("should return nothing when filtering by mobile only", () => {
@@ -81,11 +99,39 @@ describe("filter", () => {
     expect(deals).toEqual([]);
   });
 
-  it("should return only 3 deals for Plusnet when filtering by Plusnet", () => {});
+  it("should return only 3 deals for Plusnet when filtering by Plusnet", () => {
+    // Act
+    store.setProviderFilter(getProviderId("plusnet"));
+    const { deals } = store;
 
-  it("should return the 1 deal for EE only when filtering by EE", () => {});
+    // Assert
+    expect(deals).toHaveLength(3);
+  });
 
-  it("should return 5 deals for BT only when filtering by BT", () => {});
+  it("should return the 1 deal for EE only when filtering by EE", () => {
+    // Act
+    store.setProviderFilter(getProviderId("ee"));
+    const { deals } = store;
 
-  it("should return the 1 deal for Origin Broadband only when filtering by Origin Broadband", () => {});
+    // Assert
+    expect(deals).toHaveLength(1);
+  });
+
+  it("should return 5 deals for BT only when filtering by BT", () => {
+    // Act
+    store.setProviderFilter(getProviderId("bt"));
+    const { deals } = store;
+
+    // Assert
+    expect(deals).toHaveLength(5);
+  });
+
+  it("should return the 1 deal for Origin Broadband only when filtering by Origin Broadband", () => {
+    // Arrange
+    store.setProviderFilter(getProviderId("origin broadband"));
+    const { deals } = store;
+
+    // Assert
+    expect(deals).toHaveLength(1);
+  });
 });
