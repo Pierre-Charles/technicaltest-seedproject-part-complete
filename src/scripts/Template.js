8254dc8353d015d8469723fe72a54ccf5cfc2e16
Pierre Charles <p.charles0501@gmail.com>
2021-05-03 18:04:33 +0100

    Adding new monthly cost template, adding switch case for logos

--- src/scripts/Template.js
+++ src/scripts/Template.js
@@ -6,23 +6,36 @@ class Template {
     this.currencyFormatter = new Intl.NumberFormat("en-GB", {
       style: "currency",
       currency: "GBP",
-      minimumFractionDigits: 2
+      minimumFractionDigits: 2,
     });
   }
 
   buildDeal(data) {
     return this.deal
+      .replace("{{ image }}", this.getImage(data.provider.id))
       .replace("{{ title }}", data.title)
       .replace("{{ provider }}", data.provider.name)
       .replace(
         "{{ upfrontCost }}",
-        data.cost.upfrontCost > 0 ? this.currencyFormatter.format(data.cost.upfrontCost) : 'None.'
+        data.cost.upfrontCost > 0
+          ? this.currencyFormatter.format(data.cost.upfrontCost)
+          : "No"
       )
       .replace(
         "{{ totalCost }}",
         this.currencyFormatter.format(data.cost.totalContractCost)
       )
       .replace("{{ contractLength }}", data.contractLength + " months")
+      .replace(
+        "{{ monthlyCost }}",
+        this.currencyFormatter.format(
+          this.calculateMonthlyCost(
+            data.cost.totalContractCost,
+            data.contractLength
+          )
+        )
+      )
+      .replace("{{ ctaProviderSite }}", this.getProviderSite(data.provider.id))
       .replace(
         "{{ productList }}",
         this.buildProductIconList(data.productTypes)
@@ -42,7 +55,7 @@ class Template {
 
   buildProductIconList(list) {
     return list.reduce((result, product) => {
-      if (product === 'Phone') {
+      if (product === "Phone") {
         return result;
       }
       const iconId = this.getIconId(product);
@@ -70,6 +83,54 @@ class Template {
         break;
     }
     return `#icon-${id}`;
+  }
+
+  getImage(id) {
+    let name;
+    switch (id) {
+      case 1:
+        name = "sky";
+        break;
+      case 42:
+        name = "plusnet";
+        break;
+      case 3:
+        name = "bt";
+        break;
+      case 116:
+        name = "origin";
+        break;
+      case 48:
+        name = "ee";
+        break;
+    }
+    return `<img src='/logos/${name}.png'>`;
+  }
+
+  calculateMonthlyCost(totalCost, contractLength) {
+    return totalCost / contractLength;
+  }
+
+  getProviderSite(id) {
+    let link;
+    switch (id) {
+      case 1:
+        link = "https://www.sky.com/broadband";
+        break;
+      case 42:
+        link = "https://www.plus.net/broadband/";
+        break;
+      case 3:
+        link = "https://www.bt.com/broadband/";
+        break;
+      case 116:
+        link = "https://www.originbroadband.com/broadband";
+        break;
+      case 47:
+        link = "https://shop.ee.co.uk/broadband";
+        break;
+    }
+    return `<a href=${link} target='_blank'>Grab this deal!</a>`;
   }
 }
 
